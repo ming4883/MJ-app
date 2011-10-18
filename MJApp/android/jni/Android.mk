fcd2ce1735a24fb668c175ca479c057b5d601bff
Ka-ming Chan <ming4883@gmail.com>
2011-10-18 18:43:51 +0800

    progress on android porting

--- MJApp/android/jni/Android.mk
+++ MJApp/android/jni/Android.mk
@@ -2,19 +2,17 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := crender-water-example
+LOCAL_MODULE := mjapp
 
 #LOCAL_SRC_FILES += ../../../Water.example.c
-LOCAL_SRC_FILES += ../../../Reflection.example.c
-LOCAL_SRC_FILES += ../../../Common.c
-LOCAL_SRC_FILES += ../../../Framework.android.c
-LOCAL_SRC_FILES += ../../../Material.c
-LOCAL_SRC_FILES += ../../../Mesh.c
-LOCAL_SRC_FILES += ../../../Mesh.obj.c
-LOCAL_SRC_FILES += ../../../Pvr.c
-LOCAL_SRC_FILES += ../../../Remote.c
-LOCAL_SRC_FILES += ../../../Remote.impl.android.c
-LOCAL_SRC_FILES += ../../../Stream.c
+LOCAL_SRC_FILES += ../../MJApp.c
+LOCAL_SRC_FILES += ../../Crender/example/Common.c
+LOCAL_SRC_FILES += ../../Crender/example/Framework.android.c
+LOCAL_SRC_FILES += ../../Crender/example/Material.c
+LOCAL_SRC_FILES += ../../Crender/example/Mesh.c
+LOCAL_SRC_FILES += ../../Crender/example/Mesh.obj.c
+LOCAL_SRC_FILES += ../../Crender/example/Pvr.c
+LOCAL_SRC_FILES += ../../Crender/example/Stream.c
 
 LOCAL_STATIC_LIBRARIES := crender android_native_app_glue
 
