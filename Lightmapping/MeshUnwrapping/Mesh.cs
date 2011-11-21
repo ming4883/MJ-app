53698636ccb3837fdad014f79b82b781a423cf6a
ming <ming4883@gmail.com>
2011-11-21 22:30:08 +0800

    progress on Lightmapping

---
+++ Lightmapping/MeshUnwrapping/Mesh.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace MCD
+{
+	public class Mesh
+	{
+		List<int> indices;
+		List<Vector3> positions;
+		List<Vector3> normals;
+		List<Vector2> texcrds0;
+		List<Vector2> texcrds1;
+
+		public List<int> Indices { get { return indices; } }
+		public List<Vector3> Positions { get { return positions; } }
+		public List<Vector3> Normals { get { return normals; } }
+		public List<Vector2> Texcrds0 { get { return texcrds0; } }
+		public List<Vector2> Texcrds1 { get { return texcrds1; } }
+
+		public int FaceCount { get { return (null == indices) ? 0 : indices.Count / 3; } }
+		public int VertexCount { get { return (null == positions) ? 0 : positions.Count; } }
+
+		public void Init(int icnt, int vcnt)
+		{
+			indices = new List<int>();
+			for (int i = 0; i < icnt; ++i)
+				indices.Add(0);
+
+			positions = new List<Vector3>(vcnt);
+			normals = new List<Vector3>(vcnt);
+			texcrds0 = new List<Vector2>(vcnt);
+			texcrds1 = new List<Vector2>(vcnt);
+
+			for (int i = 0; i < vcnt; ++i)
+			{
+				positions.Add(Vector3.Zero);
+				normals.Add(Vector3.Zero);
+				texcrds0.Add(Vector2.Zero);
+				texcrds1.Add(Vector2.Zero);
+			}
+		}
+
+		public int FaceOffset(int faceIndex)
+		{
+			return faceIndex * 3;
+		}
+
+		public void FacePositions(out Vector3 p0, out Vector3 p1, out Vector3 p2, int faceIndex)
+		{
+			int off = FaceOffset(faceIndex);
+			p0 = positions[indices[off + 0]];
+			p1 = positions[indices[off + 1]];
+			p2 = positions[indices[off + 2]];
+		}
+
+		public void FaceNormals(out Vector3 p0, out Vector3 p1, out Vector3 p2, int faceIndex)
+		{
+			int off = FaceOffset(faceIndex);
+			p0 = normals[indices[off + 0]];
+			p1 = normals[indices[off + 1]];
+			p2 = normals[indices[off + 2]];
+		}
+	}
+}
---
+++ Lightmapping/MeshUnwrapping/MeshUnwrapper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace MCD
+{
+	public interface IMeshUnwrapper
+	{
+		void Unwrap(Mesh mesh, float scale);
+	}
+
+	public class PreFaceUnwrapper : IMeshUnwrapper
+	{
+		protected delegate Vector2 TC(Vector3 p, float scale);
+
+		protected static TC X_MAJOR = delegate(Vector3 p, float scale) { return new Vector2(p.Z * scale, p.Y * scale); };
+		protected static TC Y_MAJOR = delegate(Vector3 p, float scale) { return new Vector2(p.X * scale, p.Z * scale); };
+		protected static TC Z_MAJOR = delegate(Vector3 p, float scale) { return new Vector2(p.X * scale, p.Y * scale); };
+
+		protected class FaceUV
+		{
+			public Vector2[] Texcoord = new Vector2[3];
+
+			public void ZeroOffset()
+			{
+				Vector2 min = Texcoord[0], max = Texcoord[0];
+
+				for (int i = 1; i < Texcoord.Length; ++i)
+				{
+					min = Vector2.Min(min, Texcoord[i]);
+					max = Vector2.Max(max, Texcoord[i]);
+				}
+
+				for (int i = 0; i < Texcoord.Length; ++i)
+					Texcoord[i] -= min;
+			}
+		}
+
+		protected static TC GetMajorAxis(ref Vector3 n)
+		{
+			float absx = Math.Abs(n.X);
+			float absy = Math.Abs(n.Y);
+			float absz = Math.Abs(n.Z);
+
+			if (absx > absy)
+			{
+				if (absx > absz)
+					return X_MAJOR;
+				else
+					return Z_MAJOR;
+			}
+			else
+			{
+				if (absy > absz)
+					return Y_MAJOR;
+				else
+					return Z_MAJOR;
+			}
+		}
+
+		public void Unwrap(Mesh mesh, float scale)
+		{
+			List<FaceUV> faceuvs = new List<FaceUV>();
+
+			int fcnt = mesh.FaceCount;
+			for (int i = 0; i < fcnt; ++i)
+			{
+				Vector3 p0, p1, p2, e1, e2;
+				
+				// computer the face normal
+				mesh.FacePositions(out p0, out p1, out p2, i);
+
+				e1 = p1 - p0; e1.Normalize();
+				e2 = p2 - p0; e2.Normalize();
+
+				Vector3 n = Vector3.Cross(e1, e2);
+				n.Normalize();
+
+				// get major axis & assign texcoord
+				TC tc = GetMajorAxis(ref n);
+
+				FaceUV faceuv = new FaceUV();
+				faceuv.Texcoord[0] = tc(p0, scale);
+				faceuv.Texcoord[1] = tc(p1, scale);
+				faceuv.Texcoord[2] = tc(p2, scale);
+				faceuv.ZeroOffset();
+
+				faceuvs.Add(faceuv);
+			}
+		}
+	}
+}
